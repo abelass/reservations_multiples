2bb70ccc088cf3ac174f07c53d8698bffcc5321f
Rainer <rainer@websimple.be>
2014-05-07 18:04:25 +0200

    import initial

---
+++ reservations_multiples_autorisations.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Définit les autorisations du plugin Réservations multiples
+ *
+ * @plugin     Réservations multiples
+ * @copyright  2014
+ * @author     Rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservations_multiples\Autorisations
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/*
+ * Un fichier d'autorisations permet de regrouper
+ * les fonctions d'autorisations de votre plugin
+ */
+
+/**
+ * Fonction d'appel pour le pipeline
+ * @pipeline autoriser */
+function reservations_multiples_autoriser(){}
+
+
+/* Exemple
+function autoriser_configurer_reservations_multiples_dist($faire, $type, $id, $qui, $opt) {
+	// type est un objet (la plupart du temps) ou une chose.
+	// autoriser('configurer', '_reservations_multiples') => $type = 'reservations_multiples'
+	// au choix
+	return autoriser('webmestre', $type, $id, $qui, $opt); // seulement les webmestres
+	return autoriser('configurer', '', $id, $qui, $opt); // seulement les administrateurs complets
+	return $qui['statut'] == '0minirezo'; // seulement les administrateurs (même les restreints)
+	// ...
+}
+*/
+
+
+
+?>
