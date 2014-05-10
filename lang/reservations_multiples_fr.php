5bcecd8188e9292e8f87ff4813399f8e36f94673
Rainer <rainer@websimple.be>
2014-05-10 12:56:41 +0200

    ajouter des champs

--- lang/reservations_multiples_fr.php
+++ lang/reservations_multiples_fr.php
@@ -4,17 +4,21 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	// R
-	'reservations_multiples_titre' => 'Réservations multiples',
-
 	// C
 	'cfg_titre_parametrages' => 'Paramétrages',
+	
+	//L
+	'label_ajouter'=>'Ajouter',	
+	'label_ajouter'=>'Enlever',		
+	'label_reserver_amie'=>'J\'aimerai réserver pour une autre persone :',
 	
 	//N
 	'nombre_limite' => 'Nombre limites',
 	'nombre_limite_explication' => 'Si vide aucune limitation',	
-
+	
+	// R
+	'reservations_multiples_titre' => 'Réservations multiples',
+	
 	// T
 	'titre_page_configurer_reservations_multiples' => 'Réservations multiples',
 );
--- reservations_multiples_pipelines.php
+++ reservations_multiples_pipelines.php
@@ -15,16 +15,44 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function reservations_multiples_formulaire_charger($flux){
 	$form = $flux['args']['form'];
 	if ($form=='reservation'){
-	$flux['data']['auteurs']='';
-		
-	}
+		$champs_extra=false;
+
+			$champs_extra=true;
+			$champs_extras_auteurs_add=array();
+
+		$flux['data']['auteurs']='';				
+		$nombre_auteurs=_request('nombre_auteurs')?_request('nombre_auteurs'):0;
+
+		$ajouter=array();
+		$i = 1;
+		while ($i <= $nombre_auteurs) {
+			$nr=$i++;
+		    $ajouter[$nr]=$nr;
+			$flux['data']['nom_'.$nr]='';
+			$flux['data']['email_'.$nr]='';	
+			if($flux['data']['champs_extras_auteurs']){
+				//Adapter les champs extras
+		       foreach($flux['data']['champs_extras_auteurs'] as $key =>$value){
+		           $valeurs[$value['options']['nom'].'_'.$nr]='';
+				   $champs_extras_auteurs_add[$nr][$key]=$value;
+				   $champs_extras_auteurs_add[$nr][$key]['options']['label']=extraire_multi($value['options']['label']);  
+		           $champs_extras_auteurs_add[$nr][$key]['options']['nom']=$value['options']['nom'].'_'.$nr;              
+		            }
+		        }	
+			}
+		$flux['data']['nombre_auteurs']=$nombre_auteurs;
+		$flux['data']['champs_extras_auteurs_add']=$champs_extras_auteurs_add;
+		$flux['data']['ajouter']=$ajouter;
+
+		}
+			
 	return $flux;
 }
 
 function reservations_multiples_formulaire_verifier($flux){
 	$form = $flux['args']['form'];
 	if ($form=='reservation'){
-
+		if(_request('nombre_auteurs'))$flux['data']['ajouter'] = 'ajouter auteurs';
 	}
 	return $flux;
 }
@@ -39,7 +67,6 @@ function reservations_multiples_formulaire_traiter($flux){
 
 function reservations_multiples_recuperer_fond($flux){
 	$fond = $flux['args']['fond'];
-	echo $fond;
 	if ($fond=='formulaires/inc-reservation_connection'){
 		$auteurs_multiples=recuperer_fond('inclure/auteurs_multiples',$flux['data']['contexte'],array('ajax'=>'oui'));
 		$flux['data']['texte'] .=  $auteurs_multiples;
