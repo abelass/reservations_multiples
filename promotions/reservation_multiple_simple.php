6c8271ac487160af1e84c24974c0675f90e57cac
unknown <mullrai@D02DI1146441.net1.cec.eu.int>
2014-05-23 17:00:36 +0200

    enregister l promotion - manque encore la récupération de l'id_reservation_detail

--- promotions/reservation_multiple_simple.php
+++ promotions/reservation_multiple_simple.php
@@ -39,19 +39,27 @@ function promotions_reservation_multiple_simple_dist($flux=array(),$option=''){
 
 function promotions_reservation_multiple_simple_action_dist($flux,$promotion){
 		
-	$prix_original=$flux['data']['prix_ht'];
+	$prix_normal=$flux['data']['prix_ht'];
 	$reduction=$promotion['valeurs_promotion']['reduction'];
 	$type_reduction=$promotion['valeurs_promotion']['type_reduction'];
 	
 	$nr_auteur=_request('nr_auteur');
 	$nombre_auteurs=_request('nombre_auteurs');
 	
+	//Définir le prix réduit
 	if($nombre_auteurs and !$nr_auteur){
-			if($type_reduction=='pourcentage')$flux['data']['prix_ht']=$prix_original-($prix_original/100*$reduction);
-			elseif($type_reduction=='absolu')$flux['data']['prix_ht']=$prix_original-$reduction;
+			if($type_reduction=='pourcentage')$flux['data']['prix_ht']=$prix_normal-($prix_normal/100*$reduction);
+			elseif($type_reduction=='absolu')$flux['data']['prix_ht']=$prix_normal-$reduction;
 	}
 
-	//if($id_objet=$flux['data']['id_reservation_detail']) sql_insertq('spip_promotions_liens');
+	//Enregistrer l'offre
+	sql_insertq('spip_promotions_liens',array(
+		'id_promotion'=>$promotion['id_promotion'],
+		'id_objet'=>$flux['data']['id_reservation_detail'],
+		'objet'=>'reservation_detail',
+		'prix_normal'=>$prix_normal,
+		'prix_promotion'=>$flux['data']['prix_ht']	
+		));
 
     return $flux;
 }
