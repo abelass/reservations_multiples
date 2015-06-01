6028ce92d7dfcbdce7f157e838bd412fde3af341
Rainer <rainer@websimple.be>
2015-06-01 13:44:32 +0200

    selection nombre . configuration

--- lang/reservations_multiples_fr.php
+++ lang/reservations_multiples_fr.php
@@ -2,29 +2,35 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_titre_parametrages' => 'Paramétrages',
-	
+
+  // E
+  'explication_multiple_inscriptions' => 'Pour chaque événement il es possible d\'indiquer le nombre de réservations',
+  'explication_multiple_personnes' => 'Pour chaque personne aditionnelle il est alors possible/nécessaire de renseigner ses détails personnels',
+
 	//L
-	'label_ajouter'=>'Ajouter',	
-	'label_enlever'=>'Enlever',	
+	'label_ajouter'=>'Ajouter',
+	'label_enlever'=>'Enlever',
+	'label_multiple_inscriptions'=>'Permettre d\'indiquer le nombre d\'inscriptions',
+	'label_multiple_personnes'=>'Permettre l\'inscription de plusieurs personnes',
 	'label_reserver_amie'=>'J\'aimerai également inscrire une autre persone :',
-	
+
 	//M
 	'message_ok_reservations_pour'=>'Réservé pour : @noms@',
-	
+
 	//N
 	'nom_reservation_multiple_simple'=>'Réservation multiple simple',
 	'nombre_limite' => 'Nombre limites',
-	'nombre_limite_explication' => 'Si vide aucune limitation',	
-	
-	
+	'nombre_limite_explication' => 'Si vide aucune limitation',
+
+
 	// R
 	'reservations_multiples_titre' => 'Réservations multiples',
-	
+
 	// T
 	'titre_page_configurer_reservations_multiples' => 'Réservations multiples',
 );
